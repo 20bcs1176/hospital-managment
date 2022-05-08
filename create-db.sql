409f93becb2581a9c9247f11a35d2a5fce0c7edb
xenon314159 <jupadhyay02@gmail.com>
2022-05-09 01:16:28 +0530

    fixed sql error statments

--- create-db.sql
+++ create-db.sql
@@ -1,19 +1,20 @@
-CREATE DATABASE hospital;
+CREATE DATABASE IF NOT EXISTS hospital;
 
 USE hospital;
 
-CREATE TABLE Doctors(
-    Doctor_ID int NOT NULL AUTO_INCREMENT=100,
+CREATE TABLE IF NOT EXISTS Doctors(
+    Doctor_ID int NOT NULL AUTO_INCREMENT,
     First_name VARCHAR(255),
     Last_name VARCHAR(255),
     Age int,
     Phone VARCHAR(15),
-    Dept. VARCHAR(255),
+    Dept VARCHAR(255),
     PRIMARY KEY(Doctor_ID)
 );
+ALTER TABLE Doctors AUTO_INCREMENT = 100;
 
-CREATE TABLE Patients(
-    Patient_ID int not NULL AUTO_INCREMENT=1000,
+CREATE TABLE IF NOT EXISTS Patients(
+    Patient_ID int not NULL AUTO_INCREMENT,
     First_name VARCHAR(255),
     Last_name VARCHAR(255),
     Age int,
@@ -26,19 +27,21 @@ CREATE TABLE Patients(
     FOREIGN KEY(Doctor_ID) REFERENCES Doctors(Doctor_ID)
 );
 
-CREATE TABLE Credentials (
+ALTER TABLE Patients AUTO_INCREMENT = 1000;
+
+CREATE TABLE IF NOT EXISTS Credentials (
     First_name VARCHAR(255) NOT NULL,
     Last_name VARCHAR(255),
     Email VARCHAR(255) NOT NULL,
     Password VARCHAR(255) NOT NULL
 );
 
-CREATE VIEW Doctors_Name AS 
+CREATE VIEW IF NOT EXISTS Doctors_Name AS 
     SELECT First_name, Last_name FROM Doctors;
-CREATE VIEW Patients_Name AS
+CREATE VIEW IF NOT EXISTS Patients_Name AS
     SELECT First_name, Last_name FROM Patients;
 
-INSERT INTO Doctors VALUES
+INSERT INTO Doctors(First_name, Last_name, Age, Phone, Dept) VALUES
     ('Raju', 'Rastogi', 45, '9988776655', 'Cardiology'),
     ('Seema', 'Khurana', 51, '9988776654', 'Nuerology'),
     ('Nihal', 'Khamble', 51, '9988776652', 'Pediatrics');
