de145e9402ea2ce790529729e17754a9a9b39d65
xenon314159 <jupadhyay02@gmail.com>
2022-05-09 00:41:31 +0530

    added sql statments for db creation

--- create-db.sql
+++ create-db.sql
@@ -1,8 +1,44 @@
 CREATE DATABASE hospital;
 
-CREATE TABLE login_registration (
+USE hospital;
+
+CREATE TABLE Doctors(
+    Doctor_ID int NOT NULL AUTO_INCREMENT=100,
+    First_name VARCHAR(255),
+    Last_name VARCHAR(255),
+    Age int,
+    Phone VARCHAR(15),
+    Dept. VARCHAR(255),
+    PRIMARY KEY(Doctor_ID)
+);
+
+CREATE TABLE Patients(
+    Patient_ID int not NULL AUTO_INCREMENT=1000,
+    First_name VARCHAR(255),
+    Last_name VARCHAR(255),
+    Age int,
+    Phone VARCHAR(255),
+    Dept VARCHAR(255),
+    Payment_Status VARCHAR(20),
+    Doctor_ID int NOT NULL,
+
+    PRIMARY KEY(Patient_ID),
+    FOREIGN KEY(Doctor_ID) REFERENCES Doctors(Doctor_ID)
+);
+
+CREATE TABLE Credentials (
     First_name VARCHAR(255) NOT NULL,
     Last_name VARCHAR(255),
     Email VARCHAR(255) NOT NULL,
     Password VARCHAR(255) NOT NULL
 );
+
+CREATE VIEW Doctors_Name AS 
+    SELECT First_name, Last_name FROM Doctors;
+CREATE VIEW Patients_Name AS
+    SELECT First_name, Last_name FROM Patients;
+
+INSERT INTO Doctors VALUES
+    ('Raju', 'Rastogi', 45, '9988776655', 'Cardiology'),
+    ('Seema', 'Khurana', 51, '9988776654', 'Nuerology'),
+    ('Nihal', 'Khamble', 51, '9988776652', 'Pediatrics');
